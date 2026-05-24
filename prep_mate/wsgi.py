3ddbd587c3bb458c1725c1e43bad7f5379be3a8b
Aleksei <alek.fo@mail.ru>
2026-05-24 10:46:03 +0300

    fix_waiting

--- prep_mate/wsgi.py
+++ prep_mate/wsgi.py
@@ -9,9 +9,6 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 
 import os
 
-from gevent import monkey
-monkey.patch_all()
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prep_mate.settings')
